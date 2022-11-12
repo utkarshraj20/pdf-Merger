3dbcf516d96da5d751a5f89d3361fa5a88291a33
Utkarsh Rajput <rajpututkarsh148@gmail.com>
2022-11-12 16:44:52 +0530

    testing

--- merge.js
+++ merge.js
@@ -3,13 +3,13 @@ const PDFMerger = require('pdf-merger-js');
 var merger = new PDFMerger();
 
 const mergePdfs = async (p1, p2) => {
+  var merger = new PDFMerger();
   await merger.add(p1);  
   await merger.add(p2);
   let d = new Date().getTime()
   
   await merger.save(`public/${d}.pdf`);
-  return d 
-
+  return d
 } 
 
 module.exports = {mergePdfs}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+
 const multer  = require('multer')
 const {mergePdfs}  = require('./merge')
 
@@ -18,7 +19,7 @@ app.get('/', (req, res) => {
 app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
   console.log(req.files)
   let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
-  res.redirect("https://www.google.com/")
+  res.redirect(`https://pdf-merger.onrender.com/static/${d}.pdf`)
 })
 
 
